b375c0f51a04d7444b9e9d70246d776ed2982212
WildRoger <mx.sverre@gmail.com>
2026-08-12 18:38:03 +0500

    update init in netsip

--- python/netsip/src/netsip/__init__.py
+++ python/netsip/src/netsip/__init__.py
@@ -1,10 +1,20 @@
 #
-# NetSIPCore
+# NetSIP
+#
+# Python library for controlling the NetSIPCore SIP engine.
+#
 # Copyright (C) 2026 WildRogerr
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# version 2 as published by the Free Software Foundation.
+# This file is part of NetSIP.
+#
+# NetSIP is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2
+# as published by the Free Software Foundation.
+#
+# NetSIP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
 #
 
 __version__ = "1.1.1"
